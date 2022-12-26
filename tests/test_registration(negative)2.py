2afa1733ca7b8984f7970c3b226d7fe5d3c208dc
fayustov <fayustov26@mail.ru>
2022-12-26 21:00:29 +0300

    Убрал лишние отладочные данные в test_registration(negative)2.py

--- tests/test_registration(negative)2.py
+++ tests/test_registration(negative)2.py
@@ -1,5 +1,3 @@
-import time
-
 from test_data import url, email, name, inputEmail, inputName, btnSignUpInHomePage, btnSignUpPath
 from selenium.webdriver.common.by import By
 
@@ -10,5 +8,4 @@ def test_signUpWithNoPassword(browser):
     browser.find_element(By.XPATH, inputEmail).send_keys(email)
     browser.find_element(By.XPATH, inputName).send_keys(name)
     browser.find_element(By.XPATH, btnSignUpPath).click()
-    time.sleep(10)
     assert "http://localhost:5000/signup" in browser.current_url
